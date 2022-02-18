83fe9ab2023aa99e67a30eea6937d28c0dc8e4aa
Jonathan <59382134+jhutchi@users.noreply.github.com>
2022-02-17 16:12:49 -0800

    Create degree.h

---
+++ degree.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+
+enum class DegProgram { SECURITY, NETWORK, SOFTWARE };	// DegProgram enumeration recording type of degree
+static const std::string degTypeStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };//for output
