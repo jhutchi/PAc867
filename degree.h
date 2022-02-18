2ae9f69bd57fb2bdbf884cf26d67e6e7a2e91855
Jonathan <59382134+jhutchi@users.noreply.github.com>
2022-02-17 16:11:44 -0800

    Delete degree.h

--- degree.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-
-enum class DegProgram { SECURITY, NETWORK, SOFTWARE };	// DegProgram enumeration recording type of degree
-static const std::string degTypeStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };//for output
